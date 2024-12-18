f2d32384ba4778e1227895769d3b573d30e64d27
Andrea <andreainigolara@hotmail.com>
2024-12-18 23:07:53 +0100

    react counter

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Contador from "./components/Contador";
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Contador</h1>
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contador.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "../App.scss"; // Importa tu archivo de estilos
+
+function Contador() {
+  const [count, setCount] = useState(0);
+
+  const incrementar = () => {
+    if (count < 16) setCount(count + 1);
+  };
+
+  const decrementar = () => {
+    if (count > 0) setCount(count - 1);
+  };
+
+  const reiniciar = () => setCount(0);
+
+  return (
+    <div className="container">
+      <h2>Valor del contador: {count}</h2>
+      <div className="button-container">
+        <button onClick={incrementar} className="button">
+          Incrementar
+        </button>
+        <button onClick={decrementar} className="button">
+          Decrementar
+        </button>
+        <button onClick={reiniciar} className="button">
+          Reiniciar
+        </button>
+      </div>
+      {count === 16 && <p className="maximo-alerta">¡Máximo alcanzado!</p>}
+    </div>
+  );
+}
+
+export default Contador;
